d0b00f45cad62e765d46189260ce7429d90a835b
simon_ellershaw <simje@live.co.uk>
2020-12-04 13:05:16 +0000

    Add validation and test split to dataloader

--- mnistDigitLoader.py
+++ mnistDigitLoader.py
@@ -1,29 +1,44 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import math
+
 
 class MnistDigits:
     image_size = 16
 
     def __init__(self, fname):
-        data = np.genfromtxt(fname)
-        self.images = data[:, 1:]
-        self.labels = data[:, 0]
-        self.labels
-        self.length = data.shape[0]
-    
-    def __len__(self):
-        return self.length
-    
-    def visualise_sample(self, index):
-        label = self.labels[index]
-        image = self.images[index].reshape(self.image_size, self.image_size)
+        self.data = np.genfromtxt(fname)
+        self.label_column = 0
 
+    def get_split_datasets(self, fraction_test=0.2, fraction_val=0):
+        num_samples = self.data.shape[0]
+        divider_test = math.floor(fraction_test * num_samples)
+        divider_val = divider_test + math.floor(fraction_val * num_samples)
+
+        np.random.shuffle(self.data)
+        images = self.data[:, self.label_column+1:]
+        labels = self.data[:, self.label_column]
+
+        return {
+            "images_test": images[:divider_test],
+            "images_val": images[divider_test:divider_val],
+            "images_train": images[divider_val:],
+            "labels_test": labels[:divider_test],
+            "labels_val": labels[divider_test:divider_val],
+            "labels_train": labels[divider_val:]
+        }
+
+    @staticmethod
+    def visualise_sample(image, label):
+        image = image.reshape(MnistDigits.image_size, MnistDigits.image_size)
         plt.imshow(image)
-        plt.title(f"Index: {index}, Label: {label}")
+        plt.title(f"Label: {label}")
         plt.show()
-        
+
+    
+
 
 if __name__ == "__main__":
-    data = MnistDigits(r"Data\dtest123.dat")
-    data.visualise_sample(4)
+    datasets = MnistDigits(r"Data\dtest123.dat").get_split_datasets()
+    MnistDigits.visualise_sample(datasets["images_train"][0], datasets["labels_train"][0])
